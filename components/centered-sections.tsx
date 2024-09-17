4aca3da16eeb504a62074f813409e31f28466028
rmikl <robert.miklas@protonmail.com>
2024-09-17 12:28:11 +0200

    changes

--- components/centered-sections.tsx
+++ components/centered-sections.tsx
@@ -9,31 +9,31 @@ interface Section {
 
 const sections: Section[] = [
   {
-    id: 'about',
+    id: '#about',
     title: 'Profil Zawodowy',
     content: 'Jestem doświadczonym specjalistą w zakresie interwencji kryzysowej i terapii psychologicznej. Posiadam szeroką wiedzę i umiejętności w zakresie pracy z osobami dotkniętymi przemocą, problemami emocjonalnymi oraz kryzysami psychologicznymi.',
     imageUrl: '/images/profil-zawodowy.jpg',
   },
   {
-    id: 'experience',
+    id: '#experience',
     title: 'Doświadczenie Zawodowe',
     content: 'Moje doświadczenie obejmuje pracę jako konsultant ds. psychoprofilaktyki w wojsku, kierownik Ośrodka Interwencji Kryzysowej w Inowrocławiu, psycholog w Programie Pomocy Osobom Pokrzywdzonym Przestępstwem, oraz wsparcie dla policjantów w województwie kujawsko-pomorskim.',
     imageUrl: '/images/doswiadczenie.jpg',
   },
   {
-    id: 'education',
+    id: '#education',
     title: 'Wykształcenie',
     content: 'Ukończyłam studia psychologiczne w Szkole Wyższej Psychologii Społecznej w Warszawie, a także studia podyplomowe na Uniwersytecie Mikołaja Kopernika w Toruniu, Uniwersytecie im. Adama Mickiewicza w Poznaniu, Uniwersytecie Gdańskim i Uniwersytecie Wrocławskim.',
     imageUrl: '/images/wyksztalcenie.jpg',
   },
   {
-    id: 'training',
+    id: '#training',
     title: 'Kursy i Szkolenia',
     content: 'Uczestniczyłam w licznych kursach i szkoleniach, w tym z terapii przetwarzania poznawczego dla osób z PTSD, terapii traumy, terapii schematów, oraz wielu innych z zakresu psychologii, uzależnień i interwencji kryzysowej.',
     imageUrl: '/images/kursy.jpg',
   },
   {
-    id: 'support',
+    id: '#support',
     title: 'Zakres Prowadzonego Wsparcia',
     content: 'Oferuję wsparcie w zakresie interwencji kryzysowej, terapii traumy, terapii poznawczo-behawioralnej, pracy z młodzieżą, prowadzenia szkoleń i warsztatów, oraz superwizji indywidualnych i grupowych.',
     imageUrl: '/images/wsparcie.jpg',
