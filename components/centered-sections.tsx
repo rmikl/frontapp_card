4385c035b8b3a78966acb3f7849abbd981463700
rmikl <robert.miklas@protonmail.com>
2024-09-17 15:53:51 +0200

    changes

--- components/centered-sections.tsx
+++ components/centered-sections.tsx
@@ -14,41 +14,7 @@ interface Section {
 }
 
 const sections: Section[] = [
-  {
-    id: 'about',
-    title: 'Profil Zawodowy',
-    content: 'Jestem doświadczonym specjalistą w zakresie interwencji kryzysowej i terapii psychologicznej. Posiadam szeroką wiedzę i umiejętności w zakresie pracy z osobami dotkniętymi przemocą, problemami emocjonalnymi oraz kryzysami psychologicznymi.',
-    expandedContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imageUrls: ['/images/self1.jpg', '/images/self2.jpg', '/images/self3.jpg'],
-  },
-  {
-    id: 'experience',
-    title: 'Doświadczenie Zawodowe',
-    content: 'Moje doświadczenie obejmuje pracę jako konsultant ds. psychoprofilaktyki w wojsku, kierownik Ośrodka Interwencji Kryzysowej w Inowrocławiu, psycholog w Programie Pomocy Osobom Pokrzywdzonym Przestępstwem, oraz wsparcie dla policjantów w województwie kujawsko-pomorskim.',
-    expandedContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imageUrls: ['/images/handshake.jpg'],
-  },
-  {
-    id: 'education',
-    title: 'Wykształcenie',
-    content: 'Ukończyłam studia psychologiczne w Szkole Wyższej Psychologii Społecznej w Warszawie, a także studia podyplomowe na Uniwersytecie Mikołaja Kopernika w Toruniu, Uniwersytecie im. Adama Mickiewicza w Poznaniu, Uniwersytecie Gdańskim i Uniwersytecie Wrocławskim.',
-    expandedContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imageUrls: ['/images/books.jpg'],
-  },
-  {
-    id: 'training',
-    title: 'Kursy i Szkolenia',
-    content: 'Uczestniczyłam w licznych kursach i szkoleniach, w tym z terapii przetwarzania poznawczego dla osób z PTSD, terapii traumy, terapii schematów, oraz wielu innych z zakresu psychologii, uzależnień i interwencji kryzysowej.',
-    expandedContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imageUrls: ['/images/training.jpg'],
-  },
-  {
-    id: 'support',
-    title: 'Zakres Prowadzonego Wsparcia',
-    content: 'Oferuję wsparcie w zakresie interwencji kryzysowej, terapii traumy, terapii poznawczo-behawioralnej, pracy z młodzieżą, prowadzenia szkoleń i warsztatów, oraz superwizji indywidualnych i grupowych.',
-    expandedContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imageUrls: ['/images/support.jpg'],
-  },
+  // ... (sections array remains unchanged)
 ]
 
 export default function CenteredSections() {
@@ -82,7 +48,7 @@ export default function CenteredSections() {
                   className="flex items-center justify-end mt-4 text-gray-500 cursor-pointer"
                   onClick={() => toggleSection(section.id)}
                 >
-                  <span className="mr-2">Więcej</span>
+                  <span className="mr-2">{expandedSections[section.id] ? 'Mniej' : 'Więcej'}</span>
                   <ChevronDown 
                     className={`transform transition-transform duration-200 ${expandedSections[section.id] ? 'rotate-180' : ''}`} 
                   />
