cdc0991d6f730085017d2f804e27ce6cc6f7df26
rmikl <robert.miklas@protonmail.com>
2024-09-17 17:23:56 +0200

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,30 @@
-'use client';
-import ScrollingBackground from '@/components/scrolling-background';
-import ContactSection from '@/components/contact-section';
-import CenteredSections from '@/components/centered-sections';
+'use client'
+
+import { useState, useEffect } from 'react'
+import ScrollingBackground from '@/components/scrolling-background'
+import ContactSection from '@/components/contact-section'
+import CenteredSections from '@/components/centered-sections'
+import WelcomeScreen from '@/components/WelcomeScreen'
 
 export default function Home() {
+  const [showWelcome, setShowWelcome] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcome(false)
+    }, 6000) // 6 seconds
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (showWelcome) {
+    return <WelcomeScreen />
+  }
+
   return (
-    <><div id="home">
-    </div><ScrollingBackground imageUrl="/images/tlo.jpg">
+    <>
+      <div id="home"></div>
+      <ScrollingBackground imageUrl="/images/tlo.jpg">
         <div className="relative z-10">
           <CenteredSections />
           <div className="min-h-screen pb-[footer-height]">
@@ -15,6 +33,7 @@ export default function Home() {
             </section>
           </div>
         </div>
-      </ScrollingBackground></>
-);
+      </ScrollingBackground>
+    </>
+  )
 }
