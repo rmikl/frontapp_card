1210a06ec10922fedb9f5b006b2224fe4c4ed3e1
rmikl <robert.miklas@protonmail.com>
2024-09-17 09:58:27 +0200

    chagnes

--- components/HorizontalLightGrayMenu.tsx
+++ components/HorizontalLightGrayMenu.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/Button"
 import { Mail } from "lucide-react"
 import { useState, useEffect } from 'react'
 
